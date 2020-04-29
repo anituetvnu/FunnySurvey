196f2d59f5fb3d19c681b471325e50b1dd32b562
anituetvnu <ngovanan182000@gmail.com>
2020-04-29 21:36:33 +0700

    change db

--- databases/init.js
+++ databases/init.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql2');
 
 // Initialization
+//let db = mysql.createConnection({
+//    host: 'remotemysql.com',
+//    user: 'f0n8bhbA7T',
+//    password: 'LOPBaFyG3W',
+//    database: 'f0n8bhbA7T' 
+//});
 let db = mysql.createConnection({
-    host: 'remotemysql.com',
-    user: 'f0n8bhbA7T',
-    password: 'LOPBaFyG3W',
-    database: 'f0n8bhbA7T' 
+    host: 'db4free.net',
+    user: 'team_dui_admin',
+    password: '25112000',
+    database: 'team_dui',
+    port: '3306'
 });
 
 // Connect to database
